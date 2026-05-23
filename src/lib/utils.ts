161bbe1829944ccde83b0d203adf47c2edda8348
chalo72 <chalo8321@gmail.com>
2026-05-23 11:26:07 -0500

    fix: export formatCurrency from utils to fix build

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatCurrency = (value: number | string) => {
+  return new Intl.NumberFormat('es-CO', {
+    style: 'currency',
+    currency: 'COP',
+    minimumFractionDigits: 0
+  }).format(Number(value) || 0);
+};
